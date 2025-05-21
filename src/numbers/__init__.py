4791b21e731bf9c7fc894e26c354f21999fa42bb
vasu-collab <vasu2511@gmail.com>
2025-05-21 11:34:48 +0530

    first commit

---
+++ src/numbers/__init__.py
@@ -0,0 +1,6 @@
+"""This package provides functionality related to number greetings."""
+
+def hello() -> str:
+    """Return a greeting from the numbers package."""
+    return "Hello from numbers!"
+
